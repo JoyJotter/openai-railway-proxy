b2b4b441f1644de406e82580a6ccd9cf4cd79c75
kahobusiness <30713603+kahobusiness@users.noreply.github.com>
2023-04-18 15:44:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.post("/api/v1*", async (request, response) => {
   const url = "https://api.openai.com" + request.url.substring(4);
   const fetchAPI = new URL(url);
 
-  console.log('🤖️ 请求将转发至：', fetchAPI);
-
+  console.log('🤖️ 请求将转发至：\n', fetchAPI);
+  console.log(request);
 
   // 部分代理工具，请求由浏览器发起，跨域请求时会先发送一个 preflight 进行检查，也就是 OPTIONS 请求
   // 需要响应该请求，否则后续的 POST 会失败
